3fa46de0ae05ee0adbdc5a64469db8022077919b
arjun-rai <53712382+arjun-rai@users.noreply.github.com>
2021-12-31 15:15:55 -0600

    Add multiple pages

--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { 
+    BrowserRouter,
+    Routes,
+    Route         
+ } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import TimeCapsule from './TimeCapsule';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootElement = document.getElementById('root');
+ReactDOM.render(
+<BrowserRouter>
+    <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="TimeCapsule" element={<TimeCapsule />} />
+    </Routes>
+</BrowserRouter> ,
+rootElement
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
