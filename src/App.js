15ac976e57700b194f53323125e9d85242d13b06
arjun-rai <53712382+arjun-rai@users.noreply.github.com>
2021-12-31 21:25:29 -0600

    fast reload fixed

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React from "react";
 import {Link, Outlet} from "react-router-dom";
+import './App.css';
 
 export default function App() {
     return (
       <div>
-        <h1>Arjun Rai</h1>
-        <nav
+        <h1 className='container'>Arjun Rai</h1>
+        <nav className='container'
             style={{
                 borderBottom: "solid 1px",
                 paddingBottom: "1rem"
             }}
         >
            <Link to="/">Home </Link> | {" "} 
-           <Link to="/TimeCapsule">Time Capsule </Link> 
+           <Link to="/TimeCapsule">Time Capsule</Link> 
             
         </nav>
         <Outlet />
