c8f6dd6349621a27af9101e311bbff87c963ea04
arjun-rai <arjunrai.asr@gmail.com>
2025-03-17 17:08:23 -0500

    updated to make about me page

--- src/App.js
+++ src/App.js
@@ -8,23 +8,23 @@ export default function App() {
   const location = useLocation();
   return (
     <div>
-      {(!location.pathname.startsWith("/Data")) && (
-      <Navbar bg="dark" variant="dark" expand='lg'>
-        <Container>
-          <Navbar.Brand className='main-menu' href="/">Arjun Rai</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link className='main-menu' href="/">Home</Nav.Link>
-            <Nav.Link className='main-menu' href="TimeCapsule">Time Capsule</Nav.Link>
-            <Nav.Link className='main-menu' href="RobotStats">Robot Statistics</Nav.Link>
-            <Nav.Link className='main-menu' href="Data">Data</Nav.Link>
+      {/* {(!location.pathname.startsWith("/Data")) && ( */}
+      {/* <Navbar bg="dark" variant="dark" expand='lg'> */}
+        {/* <Container> */}
+          {/* <Navbar.Brand className='main-menu' href="/">Arjun Rai</Navbar.Brand> */}
+          {/* <Navbar.Toggle aria-controls="basic-navbar-nav" /> */}
+          {/* <Navbar.Collapse id="basic-navbar-nav"> */}
+          {/* <Nav className="me-auto"> */}
+            {/* <Nav.Link className='main-menu' href="/">Home</Nav.Link> */}
+            {/* <Nav.Link className='main-menu' href="TimeCapsule">Time Capsule</Nav.Link> */}
+            {/* <Nav.Link className='main-menu' href="RobotStats">Robot Statistics</Nav.Link> */}
+            {/* <Nav.Link className='main-menu' href="Data">Data</Nav.Link> */}
             {/* <Nav.Link href="PortfolioPage">Portfolio</Nav.Link> */}
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-      )}
+            {/* </Nav> */}
+          {/* </Navbar.Collapse> */}
+        {/* </Container> */}
+      {/* </Navbar> */}
+      {/* )} */}
       <Outlet /> 
     </div>
     
--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,34 @@
 import React from "react";
 import './Home.css';
 import logo from './imgs/profile.jpeg';
+import github from './imgs/github.svg';
+// import insta from './imgs/instagram.svg';
+import linkedin from './imgs/linkedin.svg';
 
 export default function Home() {
   return (
     <div className="home">
-      <img src={logo} className="logo"/> 
+      {/* <img src={logo} className="logo"/>  */}
+      <h1>Arjun Rai</h1>
+      <p>
+          I am a first-year computer science student at Rice University, passionate about building tools and applications that make a real impact.
+         <br/>
+         <br/> 
+         Previously, I developed a GPT-powered product search agent, built an ETL pipeline and recommendation system for semantic search using text embeddings, and helped create a package notification system for Rice students.
+         <br/>
+         <br/>
+         Beyond software, I enjoy hands-on building and tinkering. This past year, I designed and built a 1-lb PLA battle bot and competed in a competition. I also developed a prototype battle bot weapon testing rig for the Rice Robotics Club.
+        {/* <ul>
+        <li>test</li>
+        </ul> */}
+
+        <hr/>
+        <a href="mailto:ar255@rice.edu"><span className='center'>ar255@rice.edu</span></a>
+        <div className="logos">
+        <a href='https://github.com/arjun-rai'> <img src={github} width="50"/></a>
+        <a href='https://www.linkedin.com/in/arjun-rye/'><img src={linkedin} width="50"/></a>
+        </div>
+      </p>
     </div>
     
   );
